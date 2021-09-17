62279ae842d082bc80141d1d1ed46993557f8871
my <993577463@qq.com>
2021-09-17 09:40:08 +0800

    9.17

---
+++ dyhb/06-微信小程序/wwapp/components/nav-bar/nav-bar.js
@@ -0,0 +1,32 @@
+// components/nav-bar/nav-bar.js
+Component({
+    options: {
+        styleIsolation: 'shared'
+    },
+
+    
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+          onClickLeft: function () {
+            wx.navigateBack({
+              delta: 0,
+            })
+          }
+    }
+})
